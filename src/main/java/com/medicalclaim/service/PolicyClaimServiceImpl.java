90b692328ef5d817e5ac72cfb74feab96749f1bf
Akuthota Raghu <akuthota.raghu@hcl.com>
2019-12-11 18:30:09 +0530

    log4j added

--- src/main/java/com/medicalclaim/service/PolicyClaimServiceImpl.java
+++ src/main/java/com/medicalclaim/service/PolicyClaimServiceImpl.java
@@ -23,17 +23,20 @@ import com.medicalclaim.repository.PolicyClaimRepository;
 import com.medicalclaim.repository.PolicyRepository;
 import com.medicalclaim.repository.UserRepository;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * 
  * @author akuthota.raghu
- * @since 11-12-2019
- * This service is used  to do the implementation logic for the Raise Claim and other medical claim related things
+ * @since 11-12-2019 This service is used to do the implementation logic for the
+ *        Raise Claim and other medical claim related things
  */
 @Service
+@Slf4j
 public class PolicyClaimServiceImpl implements PolicyClaimService {
 
-	// created instance for PolicyClaimRepository to call predefined JPA repository
-	// methods
+	/* created instance for PolicyClaimRepository
+		to call predefined JPA repository methods */
 	@Autowired
 	private PolicyRepository policyRepository;
 
@@ -42,17 +45,17 @@ public class PolicyClaimServiceImpl implements PolicyClaimService {
 
 	@Autowired
 	private PolicyClaimRepository policyClaimRepository;
-	
+
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@Autowired
 	private PolicyClaimApprovalRepository policyClaimApprovalRepository;
-	
+
 	/**
 	 * @param PolicyClaimRequestDto
-	 * @return PolicyClaimResponseDto
-	 * This method is used to raise a claim and send it to first level approval
+	 * @return PolicyClaimResponseDto This method is used to raise a claim and send
+	 *         it to first level approval
 	 */
 	@Override
 	@Transactional
@@ -69,36 +72,36 @@ public class PolicyClaimServiceImpl implements PolicyClaimService {
 			policyClaim.setPolicyId(policy);
 			policyClaim.setHospitalId(hospital.get());
 
-			String claimNumber = AppConstant.CLAIM_NUMBER_PREFIX+generatePolicyClaimNumber();
+			String claimNumber = AppConstant.CLAIM_NUMBER_PREFIX + generatePolicyClaimNumber();
 			String claimNumberResponse = policyClaimRepository.findByClaimNumber(claimNumber);
-			
-			if(claimNumberResponse == null) {
+
+			if (claimNumberResponse == null) {
 				policyClaim.setClaimNumber(claimNumber);
-			}else{
-				
-				if(!claimNumberResponse.equalsIgnoreCase(claimNumber)) {
+			} else {
+
+				if (!claimNumberResponse.equalsIgnoreCase(claimNumber)) {
 					policyClaim.setClaimNumber(claimNumber);
-				}else {
-					response.setMessage(AppConstant.CLAIM_NUMBER_ALREADY_EXIST); 
+				} else {
+					response.setMessage(AppConstant.CLAIM_NUMBER_ALREADY_EXIST);
 				}
 			}
-			
+
 			BeanUtils.copyProperties(policyClaimRequestDto, policyClaim);
-			
-			PolicyClaim  savedPolicyClaim = policyClaimRepository.save(policyClaim);
-			
+
+			PolicyClaim savedPolicyClaim = policyClaimRepository.save(policyClaim);
+
 			PolicyClaimApproval policyClaimApproval = new PolicyClaimApproval();
-			
+
 			policyClaimApproval.setPolicyClaimId(savedPolicyClaim);
 			policyClaimApproval.setApproval(false);
 			policyClaimApproval.setComments(AppConstant.INITIAL_CLAIM_RAISED);
-			
+
 			User approvalLevelResponse = userRepository.findByApprovalLevel(AppConstant.APPROVAL_ID_1);
 			policyClaimApproval.setClaimApprovalId(approvalLevelResponse);
-			
+
 			policyClaimApprovalRepository.save(policyClaimApproval);
-			
-			response.setMessage(AppConstant.CLAIM_RAISED_SUCCESS); 
+
+			response.setMessage(AppConstant.CLAIM_RAISED_SUCCESS);
 			response.setStatus(AppConstant.SUCCESS);
 
 		} else {
@@ -107,7 +110,7 @@ public class PolicyClaimServiceImpl implements PolicyClaimService {
 
 		return response;
 	}
-	
+
 	/**
 	 * This below method will generate the random number with length of 8
 	 * @return Long
@@ -117,6 +120,3 @@ public class PolicyClaimServiceImpl implements PolicyClaimService {
 		return Long.valueOf(number);
 	}
 }
-
-
-
